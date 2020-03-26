31c1ae4901217440a50b6991913634d11f5fccab
Roger Santos <rogerioantoniodossantos@outlook.com>
2020-03-26 19:52:46 -0300

    Versão 1.0.0

--- index.php
+++ index.php
@@ -6,4 +6,16 @@ use sanotsroger\DigitalCep\BuscaCep;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$busca = new BuscaCep();
+try {
+    $busca = new BuscaCep();
+
+    $cep = '01001000';
+
+    $endereco = $busca->buscaEnderecoPorCep($cep);
+
+    echo '<pre>';
+    print_r($endereco);
+    echo '</pre>';
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
--- src/BuscaCep.php
+++ src/BuscaCep.php
@@ -6,8 +6,25 @@ namespace sanotsroger\DigitalCep;
 
 class BuscaCep
 {
+    const URL = 'http://viacep.com.br/ws';
+
     public function __construct()
     {
-        echo 'Busca cep';
+        // ...
+    }
+
+    public function buscaEnderecoPorCep(string $cep): array
+    {
+        $cep = preg_replace('/\D/', '', $cep);
+
+        if (strlen($cep) != 8) {
+            throw new \Exception('Tamanho do Cep incorreto. Cep: ' . $cep);
+        }
+
+        $busca = file_get_contents($this::URL . DIRECTORY_SEPARATOR . $cep . DIRECTORY_SEPARATOR . 'json');
+
+        $resultado = (array) json_decode($busca);
+
+        return $resultado;
     }
 }
